3c1e309cee741c32820067690a552e89dfb92995
sdabrowski <sdabrow2@gmail.com>
2017-05-22 14:32:42 -0500

    Add project files.

---
+++ SimonStore/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimonStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        
+        // GET: Product
+        public ActionResult Index(int? id, string productName)
+        {
+            if (id == 300)
+            {
+                return Redirect("/");
+            }
+
+            if (string.IsNullOrEmpty(productName))
+            {
+                productName = "My Product";
+            }
+
+            var product = new { id = id, name = "My Product", price = 299m, description = "This is a product" };
+            return Json(product, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Index(int? id)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
---
+++ SimonStore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SimonStore.MvcApplication" Language="C#" %>
